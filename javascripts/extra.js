f465cfa65cfe7df296fcf530aa61a5d47f7a908c
Michael Bykovski <michael.bykovskii@aoe.com>
2020-12-12 18:24:44 +0100

    Deployed 29f5727 with MkDocs version: 1.1.2

--- javascripts/extra.js
+++ javascripts/extra.js
@@ -1,11 +1,9 @@
-window.onload = (event) => {
-  const { schema, primary, accent } = JSON.parse(
-    localStorage.getItem('currentColor')
-  );
-  document.body.setAttribute('data-md-color-scheme', schema);
-  document.body.setAttribute('data-md-color-primary', primary);
-  document.body.setAttribute('data-md-color-accent', accent);
-};
+const { schema, primary, accent } = JSON.parse(
+  localStorage.getItem('currentColor')
+);
+document.body.setAttribute('data-md-color-scheme', schema);
+document.body.setAttribute('data-md-color-primary', primary);
+document.body.setAttribute('data-md-color-accent', accent);
 
 const buttons = document.querySelectorAll('button[data-md-color-scheme]');
 buttons.forEach((button) => {
